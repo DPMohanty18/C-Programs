35be7cbcc422660aca659968c0ad11a6b8351e05
Durga Prasanna Mohanty <durgaprasannamohanty888@gmail.com>
2023-12-12 19:45:21 +0530

    just updated

--- function test.c
+++ function test.c
@@ -3,11 +3,12 @@ int sum(int a,int b)
 {
     return a+b;
 }
-void main()
+int main()
 {
     int x,y,z;
     printf("Enter 2 numbers:");
     scanf("%d%d",&x,&y);
     z=sum(x,y);
     printf("The result is:%d",z);
+    return 0;
 }
